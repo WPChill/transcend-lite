7ed61395be8d5db6e0e422b5ca589693a0b09c64
Giucu <giucu91@yahoo.com>
2018-04-02 21:21:34 +0300

    Added ShortPixel Affiliate Code

--- functions.php
+++ functions.php
@@ -27,3 +27,7 @@ require_once($include_path.'setup.php');
 if ( ! class_exists( 'CPO_Theme' ) ) {
 	require get_template_directory() . '/includes/class-cpo-theme.php';
 }
+
+if ( ! defined( 'SHORTPIXEL_AFFILIATE_CODE' ) ) {
+    define( 'SHORTPIXEL_AFFILIATE_CODE', '3AXNUKA28044' );
+}
